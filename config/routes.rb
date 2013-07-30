e7aa00d5940385bec317c29b6b781f3fe63dc687
Roneesh Vashisht <roneesh@gmail.com>
2013-07-30 14:21:42 -0400

    controller and pages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 NoviSplash::Application.routes.draw do
+  
+  get "/splash", controller: "pages", action: "splash"
+  get "/confirmation", controller: "pages", action: "confirmation"
+
+  root :to => "pages#splash"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get splash" do
+    get :splash
+    assert_response :success
+  end
+
+  test "should get confirmation" do
+    get :confirmation
+    assert_response :success
+  end
+
+end
