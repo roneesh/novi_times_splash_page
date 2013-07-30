09afb74f5585ff34361e49c89fc34d6d61d974b7
Roneesh Vashisht <roneesh@gmail.com>
2013-07-30 14:15:44 -0400

    initial commit with propercss

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NoviSplash::Application.config.secret_key_base = 'eed61c6c707f8ca4b21ac9c4acc3a2fb72c257922ce3d892fe63c1f48cd19ef1e8ea7e375b6cc433e3ce4ec0453d89618303cb085cb49670758ba14dd761bb7a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NoviSplash::Application.config.session_store :cookie_store, key: '_novi-splash_session'
