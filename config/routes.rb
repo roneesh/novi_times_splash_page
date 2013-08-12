183e1c9060911338a4bb4eae637c12c7006087fe
Roneesh Vashisht <roneesh@gmail.com>
2013-08-12 11:51:48 -0400

    adding split route and password protecting it

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ NoviSplash::Application.routes.draw do
   get "/splash", controller: "pages", action: "splash"
   get "/confirmation", controller: "pages", action: "confirmation", as: "confirmation"
   get '/confirmation_share', controller: 'pages', action: 'confirmation_share', as: 'confirmation_share'
-  
+  mount Split::Dashboard, :at => 'split'
 
   root :to => "pages#splash"
   
