39341ea5054782686c7c7599c0309cfead4dd764
Roneesh Vashisht <roneesh@gmail.com>
2013-08-12 21:25:08 -0400

    commenting out javascripts

--- app/assets/javascripts/share.js
+++ app/assets/javascripts/share.js
@@ -1,26 +1,26 @@
-var twitter = false;
-var facebook = false;
+// var twitter = false;
+// var facebook = false;
 
-var appendCheck = function() {
-  console.log('I was checked!')
-  if (twitter == true && facebook == true) {
-    $('.confirm').append('<p> Hello world! </p>');
-  }
-}
+// var appendCheck = function() {
+//   console.log('I was checked!')
+//   if (twitter == true && facebook == true) {
+//     $('.confirm').append('<p> Hello world! </p>');
+//   }
+// }
 
 
 
-$( document ).ready(function() {
+// $( document ).ready(function() {
 
-  $('.twitter-share-button').click(function(){ 
-    twitter = true;
-    appendCheck();
-  });
+//   $('.twitter-share-button').click(function(){ 
+//     twitter = true;
+//     appendCheck();
+//   });
 
-  $('.facebook-button').click(function(){ 
-    facebook = true;
-    appendCheck();
-    // console.log('facebook = ' + facebook); 
-  });
+//   $('.facebook-button').click(function(){ 
+//     facebook = true;
+//     appendCheck();
+//     // console.log('facebook = ' + facebook); 
+//   });
 
-});
+// });
